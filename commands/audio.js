395aef50c51a7181d78abba3de3b2360b0375258
USMAN-SER <149039515+usmanser123@users.noreply.github.com>
2023-11-11 22:28:50 +0500

    ᴜsᴍᴀɴ-ᴍᴅ

--- commands/audio.js
+++ commands/audio.js
@@ -1,12 +1,21 @@
 /**
- Copyright (C) 2022.
- Licensed under the  GPL-3.0 License;
- You may not use this file except in compliance with the License.
- It is supplied in the hope that it may be useful.
- * @project_name : Secktor-Md
- * @author : SamPandey001 <https://github.com/SamPandey001>
- * @description : Secktor,A Multi-functional whatsapp bot.
- * @version 0.0.6
+===========================================================================================//
+//    ██╗   ██╗███████╗███╗   ███╗ █████╗ ███╗   ██╗    ███╗   ███╗██████╗        //
+//    ██║   ██║██╔════╝████╗ ████║██╔══██╗████╗  ██║    ████╗ ████║██╔══██╗      //
+//    ██║   ██║███████╗██╔████╔██║███████║██╔██╗ ██║    ██╔████╔██║██║  ██║      //
+//    ██║   ██║╚════██║██║╚██╔╝██║██╔══██║██║╚██╗██║    ██║╚██╔╝██║██║  ██║      //
+//    ╚██████╔╝███████║██║ ╚═╝ ██║██║  ██║██║ ╚████║    ██║ ╚═╝ ██║██████╔╝      //
+//     ╚═════╝ ╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝    ╚═╝     ╚═╝╚═════╝         //
+===========================================================================================//
+ Copyright (C) 2022.                                                                                        
+ Licensed under the  GPL-3.0 License;                                                      
+ You may not use this file except in compliance with the License.    
+ It is supplied in the hope that it may be useful                                     
+ * @project_name : USMAN-MD                                                                   
+ * @author : usmanser123 <https://github.com/usmanser123>   
+ * @description : USMAN-MD,A Multi-functional whatsapp bot.       
+ * @version 1.0.1                                                                                             
+ ========================================================
  **/
 
  const { tlang, ffmpeg,cmd } = require('../lib')
